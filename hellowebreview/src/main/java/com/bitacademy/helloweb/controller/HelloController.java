112f7a45acd77276382ff8a525e9971090477e32
tjdgns0029 <whtjdgns0029@naver.com>
2022-11-15 10:49:54 +0900

    @RequestMapping

---
+++ hellowebreview/src/main/java/com/bitacademy/helloweb/controller/HelloController.java
@@ -0,0 +1,29 @@
+package com.bitacademy.helloweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println("name:" + name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name", name);
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		
+		return mav;
+	}
+}
